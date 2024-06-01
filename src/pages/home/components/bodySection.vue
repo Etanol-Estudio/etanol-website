c5d7563cd76b65c1293e6a60d40d477e69553c88
Anttonino <victor.antonino12@gmail.com>
2024-06-01 18:51:38 -0300

    feat: create body section component

---
+++ src/pages/home/components/bodySection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-black">
+    <div class="p-8 flex flex-col">
+      <div class="text-7xl text-left text-white pb-4">Membros</div>
+      <cards />
+    </div>
+  </div>
+</template>
+
+<script>
+import cards from "../../../components/shared/MemberCard.vue";
+
+export default {
+  name: "heroSection",
+  components: {
+    cards,
+  },
+};
+</script>
