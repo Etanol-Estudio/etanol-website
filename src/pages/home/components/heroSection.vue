91f3e11ac21c72d7adbbdce1e107385e19b280f5
Anttonino <victor.antonino12@gmail.com>
2024-06-01 18:51:54 -0300

    feat: create hero section component

---
+++ src/pages/home/components/heroSection.vue
@@ -0,0 +1,75 @@
+<template>
+  <div v-if="$q.screen.gt.xs" class="teste h-128 w-full">
+    <div class="flex justify-between">
+      <div class="px-12 py-14 max-w-lg">
+        <div class="text-base text-white pb-4">LOREM IPSUM</div>
+        <div class="text-4xl text-white">Lorem ipsum dolor sit</div>
+        <div class="text-4xl text-white pb-5">Sed quia accusamus vel</div>
+        <div class="text-base text-white">
+          Lorem ipsum dolor sit amet. Sed quia accusamus vel temporibus incidunt
+          eos inventore saepe qui adipisci dicta. Hic iusto voluptas qui
+        </div>
+        <q-btn
+          push
+          class="q-mt-xl"
+          color="red"
+          text-color="black"
+          unelevated
+          to="/chatbot"
+          label="Click em mim"
+          no-caps
+        ></q-btn>
+      </div>
+      <!--
+        <img
+          src="../../../assets/logoEtanol.png"
+          class="md:h-64 md:mt-14 lg:h-1/3 lg:mt-5 mr-8"
+        />
+      -->
+      <img src="../../../assets/EtanolLogo.png" class="h-96 mt-2 mr-28" />
+    </div>
+  </div>
+
+  <div v-else style="background-color: #1c1c1c">
+    <div class="p-8 flex flex-col">
+      <div class="text-xs text-center text-white pb-4">LOREM IPSUM</div>
+      <div class="text-2xl text-center text-white">Lorem ipsum dolor sit</div>
+      <div class="text-2xl text-center text-white pb-5">
+        Sed quia accusamus vel
+      </div>
+      <div class="text-xs text-center text-white">
+        Lorem ipsum dolor sit amet. Sed quia accusamus vel temporibus incidunt
+        eos inventore saepe qui adipisci dicta. Hic iusto voluptas qui
+      </div>
+      <q-btn
+        push
+        class="q-mt-md"
+        color="red"
+        text-color="black"
+        unelevated
+        to="/chatbot"
+        label="Iniciar Teste"
+        no-caps
+      ></q-btn>
+    </div>
+    <div class="h-96 w-full flex justify-center">
+      <img src="../../../assets/EtanolLogo.png" class="h-96" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "heroSection",
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Clicker+Script&family=Montserrat:wght@100;400;500;800&family=Poppins:wght@300;500;600&display=swap");
+
+.teste {
+  background-image: url("../../../assets/Fundo.png");
+  background-size: cover;
+  background-position: center;
+}
+</style>
